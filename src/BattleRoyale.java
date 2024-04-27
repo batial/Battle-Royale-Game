aa9ea6013676784c51641b177a7bb6de43308f25
Sebastian <sebastian.debesa@hotmail.com>
2024-04-27 15:13:42 -0300

    update BattleRoyale, powerUps now is functional

--- src/BattleRoyale.java
+++ src/BattleRoyale.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class BattleRoyale {
     static boolean arenaCreada = false;
+    static boolean powerUpActivo = false;
 
 
     //da al usuario a elegir el tamanio de la arena
@@ -20,6 +21,13 @@ public class BattleRoyale {
         }
     }
 
+    public static boolean comprobarPowerUp(int indexVertocal, int indexHorizontal) {
+        if (Arena.posiciones[indexVertocal][indexHorizontal] == 'E') {
+            return true;
+        }
+        return false;
+    }
+
     public static void movimiento(char direccion) {
         //encuentra al jugador del turno
         int posiconVertical = 0;
@@ -36,20 +44,24 @@ public class BattleRoyale {
         }
         //direcciones de movimiento
         //verifica si el movmiento es posible
-        if (direccion == 'd' && posiconHorizontal < Arena.posiciones[0].length -1) {
+        if (direccion == 'd' && posiconHorizontal < Arena.posiciones[0].length - 1) {
             Arena.posiciones[posiconVertical][posiconHorizontal] = '-';
+            powerUpActivo = comprobarPowerUp(posiconVertical, posiconHorizontal + 1);
             Arena.posiciones[posiconVertical][posiconHorizontal + 1] = 'J';
         }
         if (direccion == 'a' && posiconHorizontal != 0) {
             Arena.posiciones[posiconVertical][posiconHorizontal] = '-';
+            powerUpActivo = comprobarPowerUp(posiconVertical, posiconHorizontal - 1);
             Arena.posiciones[posiconVertical][posiconHorizontal - 1] = 'J';
         }
-        if (direccion == 's' && posiconVertical < Arena.posiciones.length-1) {
+        if (direccion == 's' && posiconVertical < Arena.posiciones.length - 1) {
             Arena.posiciones[posiconVertical][posiconHorizontal] = '-';
+            powerUpActivo = comprobarPowerUp(posiconVertical + 1, posiconHorizontal);
             Arena.posiciones[posiconVertical + 1][posiconHorizontal] = 'J';
         }
         if (direccion == 'w' && posiconVertical != 0) {
             Arena.posiciones[posiconVertical][posiconHorizontal] = '-';
+            powerUpActivo = comprobarPowerUp(posiconVertical - 1, posiconHorizontal);
             Arena.posiciones[posiconVertical - 1][posiconHorizontal] = 'J';
         }
         System.out.println("Ingresa un movimiento valido.");
@@ -60,22 +72,29 @@ public class BattleRoyale {
         while (!arenaCreada) {
             eleccionArena();
         }
-
+        int turno = 0;
         if (Jugador.turno) {
-            Scanner sc = new Scanner(System.in);
+
+
             while (Jugador.turno) {
                 try {
+                    Scanner sc = new Scanner(System.in);
                     System.out.println("Tu turno.");
                     System.out.println("usa w a s d para moverte, o f para disparar:");
                     char movimiento = sc.next().charAt(0);
                     movimiento(movimiento);
                     Arena.mostrarArena();
+                    //usa el powerUp si esta activo
+                    if (powerUpActivo) {
+                        powerUpActivo = false;
+                        continue;
+                    }
+                    System.out.println("El turno es el:" + turno++);
                 } catch (IndexOutOfBoundsException e) {
                     System.out.println("Parece que estas queriendo acceder a una posicion invalida " + e.getMessage());
                     continue;
                 } catch (Exception e) {
                     System.out.println("ocurrio un error" + e.getMessage());
-
                 }
 
             }
